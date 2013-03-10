59581c40b9aefe4bc6bc9e672592cc156ad74042
robinbj86 <robin.jerome@outlook.com>
2013-03-10 12:37:49 +0200

    Test Commit to see if the stuff works

--- IoTPublishShare/src/com/aalto/protocol/design/iotps/utils/IoTUtils.java
+++ IoTPublishShare/src/com/aalto/protocol/design/iotps/utils/IoTUtils.java
@@ -1,5 +1,8 @@
 package com.aalto.protocol.design.iotps.utils;
 
 public class IoTUtils {
+	public static void main(String[] args) {
+		System.out.println("Test commit to see if the code works");
+	}
 
 }
