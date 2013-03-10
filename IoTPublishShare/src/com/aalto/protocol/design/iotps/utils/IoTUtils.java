520d3822c9126ecf3be8dafec870f031523afd6c
robinbj86 <robin.jerome@outlook.com>
2013-03-10 12:40:09 +0200

    Checking secure storage of password

--- IoTPublishShare/src/com/aalto/protocol/design/iotps/utils/IoTUtils.java
+++ IoTPublishShare/src/com/aalto/protocol/design/iotps/utils/IoTUtils.java
@@ -2,7 +2,7 @@ package com.aalto.protocol.design.iotps.utils;
 
 public class IoTUtils {
 	public static void main(String[] args) {
-		System.out.println("Test commit to see if the code works");
+		System.out.println("Test commit to see if the code works -- Checking Auto Commit");
 	}
 
 }
