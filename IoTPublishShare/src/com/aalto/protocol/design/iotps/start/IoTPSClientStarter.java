daeb8796402dbb05b7eec3c8975f05e36c618e06
robinbj86 <robin.jerome@outlook.com>
2013-05-16 15:11:11 +0300

    Added skeleton classes for Client & Server Starter

---
+++ IoTPublishShare/src/com/aalto/protocol/design/iotps/start/IoTPSClientStarter.java
@@ -0,0 +1,14 @@
+package com.aalto.protocol.design.iotps.start;
+
+public class IoTPSClientStarter {
+	
+	public static int version = 1;
+	
+	/* Arguments to the script
+	 * ./iot-client –s <server ip> -p <server port> [list of sensor ids]
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ IoTPublishShare/src/com/aalto/protocol/design/iotps/start/IoTPSServerStarter.java
@@ -0,0 +1,13 @@
+package com.aalto.protocol.design.iotps.start;
+
+public class IoTPSServerStarter {
+
+	public static int version = 1;
+
+	/* Arguments to the script
+	 * ./iot-server –p <publish port> -s <subscribe port>
+	 */
+	public static void main(String[] args) {
+
+	}
+}
