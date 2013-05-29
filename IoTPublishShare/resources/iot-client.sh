aeb79c631c850036a8f795b8ba7c5860d5393dca
robinbj86 <robin.jerome@outlook.com>
2013-05-29 18:48:55 +0300

    Modified scripts

--- IoTPublishShare/resources/iot-client.sh
+++ IoTPublishShare/resources/iot-client.sh
@@ -8,18 +8,19 @@ echo "##########################################################################
 # define usage function
 usage(){
 	echo "Incorrect Usage"
-	echo "Usage: ./iot-client <server ip> <server port> <version> [list of sensor ids]"
+	echo "Usage: ./iot-client <server ip> <server port> <client port> <version> [list of sensor ids]"
 	echo "                                or alternatively                            "
-	echo "./iot-client <server ip> <server port> <version> find                       "
+	echo "       ./iot-client <server ip> <server port> <client port> <version> find                "
+	echo "Example: ./iot-client.sh 127.0.0.1 5061 5070 1 find                              "
 }
 
 curr_time=`date +%s`
+echo "Starting client at :"$curr_time
 mkdir ./logs 1>./null 2>./null
-
 # You need atleast arguments to the shell script
-if [ $# -lt 4 ]
+if [ $# -lt 5 ]
 then
-  usage()
+  usage
   exit 1
 fi
 
@@ -27,12 +28,13 @@ if [ $4 == "find" ]
 then
   echo "Searching for available sensors in the server with version:" $3
   echo "Results will be shown in ten seconds"
-  java -cp iotps.jar com.aalto.protocol.design.iotps.start.IoTPSClientStarter 
+  java -cp iotps.jar com.aalto.protocol.design.iotps.start.IoTPSClientStarter $@ 
   sleep 10
   exit 1
 else
   echo "Subscribing to server with IP:"$1 " Port:" $2
-  java -cp iotps.jar com.aalto.protocol.design.iotps.start.IoTPSClientStarter $@ 1>./logs/client_info_$curr_time.log 2>./logs/client_error_$curr_time.log &
+  #java -cp iotps.jar com.aalto.protocol.design.iotps.start.IoTPSClientStarter $@ 1>./logs/client_info_$curr_time.log 2>./logs/client_error_$curr_time.log &
+  java -cp iotps.jar com.aalto.protocol.design.iotps.start.IoTPSClientStarter $@
 fi
 
 
--- IoTPublishShare/resources/iot-server.sh
+++ IoTPublishShare/resources/iot-server.sh
@@ -10,16 +10,19 @@ usage(){
 
 	echo "Incorrect Usage"
 	echo "Usage: ./iot-server <publish port> <subscribe port> <version>"
+	echo "Example: ./iot-server 5060 5061 1                            "
 }
 curr_time=`date +%s`
+# dbUrl=jdbc:sqlite:/u/92/babujer1/unix/IoTPSAssignment/db/iotps
 dbUrl=jdbc:sqlite:D:\\Software\\sqlite-shell-win32-x86-3071700\\iotps
 mkdir ./logs 1>./null 2>./null
 
 # You need exactly 3 arguments to the shell script
 if [ $# -ne 3 ]
 then
-  usage()
+  usage
   exit 1
 fi
 
-java -cp iotps.jar com.aalto.protocol.design.iotps.start.IoTPSServerStarter $@ $dbUrl 1>./logs/server_info_$curr_time.log 2>./logs/server_error_$curr_time.log &
+#java -cp iotps.jar sqlite-jdbc-3.7.2.jar com.aalto.protocol.design.iotps.start.IoTPSServerStarter $1 $2 5062 $3 $dbUrl 1>./logs/server_info_$curr_time.log 2>./logs/server_error_$curr_time.log &
+java -cp iotps.jar:sqlite-jdbc-3.7.2.jar com.aalto.protocol.design.iotps.start.IoTPSServerStarter $1 $2 5062 $3 $dbUrl 
