9b77e51ba62a7e2739fec689030d5267b85c76c8
robinbj86 <robin.jerome@outlook.com>
2013-05-29 18:55:10 +0300

    Clean up script
    Clean up

---
+++ IoTPublishShare/resources/cleanup.sh.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+# iot-server.sh: Shell script to start iot server
+clear
+echo "##################################################################################"
+echo "                           Clean UP                                               "
+echo "##################################################################################"
+
+rm -rf ./logs/server_*
+rm -rf ./logs/client_*
+rm -rf ./null
+
+cd db
+sqlite3 iotps 'delete from client_table'
+
+echo "Cleanup Complete"
